8c22fea870fd378e9b346ef7c4d24113ba75b025
null122 <71970238+null122@users.noreply.github.com>
2021-10-14 22:53:18 +0800

    前端ui

--- touhou-gamejam-quick-web/src/App.vue
+++ touhou-gamejam-quick-web/src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <el-header>Header</el-header>
+      <el-header>标题</el-header>
       <el-main>
-
+        <div v-for="item in 5" :key="item" class="main-box">
+          <el-row>
+            <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+            <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+          </el-row>
+        </div>
       </el-main>
     </el-container>
   </div>
@@ -30,5 +36,21 @@ export default {
   color: var(--el-text-color-primary);
   text-align: center;
   line-height: 160px;
+}
+
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 100px;
+}
+.main-box{
+  height: 100%;
+  width: 100%;
+  margin: 10px;
 }
 </style>
