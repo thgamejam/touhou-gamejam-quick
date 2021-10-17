b6a6ccb3d0f0f74048b5e1282d395a726f9b934b
strluck <s@strluck.com>
2021-10-17 22:28:27 +0800

    后端 添加对象存储预签名接口

--- touhou-gamejam-quick-server/download/internal/data/object_storage.go
+++ touhou-gamejam-quick-server/download/internal/data/object_storage.go
@@ -7,13 +7,15 @@ import (
     "github.com/aws/aws-sdk-go-v2/config"
     "github.com/aws/aws-sdk-go-v2/credentials"
     "github.com/aws/aws-sdk-go-v2/service/s3"
+    "time"
 )
 
 type ObjectStorage struct {
-    Client *s3.Client             // 对象存储服务
-    Input  *s3.ListObjectsV2Input // 服务调用的输入参数，目前只传入 bucket
+    Client *s3.Client // 对象存储服务
 
-    bucket *string
+    bucket                  *string
+    smallFileExpirationTime time.Duration //小文件到期时间
+    largeFileExpirationTime time.Duration //大文件到期时间
 }
 
 // NewObjectStorage 初始化对象存储
@@ -23,14 +25,16 @@ func NewObjectStorage(c *conf.Data) (*ObjectStorage, error) {
 
     const defaultRegion = "us-east-1"
 
-    staticResolver := aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
-        return aws.Endpoint{
-            PartitionID:       "aws",
-            URL:               c.ObjectStorage.Url, // minio url
-            SigningRegion:     defaultRegion,
-            HostnameImmutable: true,
-        }, nil
-    })
+    staticResolver := aws.EndpointResolverFunc(
+        func(service, region string) (aws.Endpoint, error) {
+            return aws.Endpoint{
+                PartitionID:       "aws",
+                URL:               c.ObjectStorage.Url, // minio url
+                SigningRegion:     defaultRegion,
+                HostnameImmutable: true,
+            }, nil
+        },
+    )
 
     cfg, err := config.LoadDefaultConfig(
         context.TODO(),
@@ -51,14 +55,43 @@ func NewObjectStorage(c *conf.Data) (*ObjectStorage, error) {
 
     client := s3.NewFromConfig(cfg)
 
-    // 选择 bucket
-    input := &s3.ListObjectsV2Input{
-        Bucket: &c.ObjectStorage.Bucket,
+    oss := &ObjectStorage{
+        Client:                  client,
+        smallFileExpirationTime: c.ObjectStorage.SmallFileExpirationTime.AsDuration(),
+        largeFileExpirationTime: c.ObjectStorage.LargeFileExpirationTime.AsDuration(),
     }
 
-    return &ObjectStorage{
-        Client: client,
-        Input:  input,
-        bucket: &c.ObjectStorage.Bucket,
-    }, nil
+    oss.bucket = aws.String(c.ObjectStorage.Bucket)
+
+    return oss, nil
+}
+
+// GetObjectURL 获取文件对象的URL
+// key: 对象路径
+// expirationTime: 过期时间
+func (o *ObjectStorage) GetObjectURL(ctx context.Context, key string, expirationTime time.Duration) (string, error) {
+    input := &s3.GetObjectInput{
+        Bucket: o.bucket,
+        Key:    aws.String(key),
+    }
+
+    psClient := s3.NewPresignClient(o.Client)
+    req, err := psClient.PresignGetObject(ctx, input,
+        func(options *s3.PresignOptions) {
+            options.Expires = expirationTime // 设置url过期时间
+        },
+    )
+    if err != nil {
+        return "", err
+    }
+
+    return req.URL, nil
+}
+
+func (o *ObjectStorage) GetSmallObjectURL(ctx context.Context, key string) (string, error) {
+    return o.GetObjectURL(ctx, key, o.smallFileExpirationTime)
+}
+
+func (o *ObjectStorage) GetLargeObjectURL(ctx context.Context, key string) (string, error) {
+    return o.GetObjectURL(ctx, key, o.largeFileExpirationTime)
 }
