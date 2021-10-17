2c5fe646f2befde020899716dac5d87856529fb4
null122 <71970238+null122@users.noreply.github.com>
2021-10-17 12:12:25 +0800

    卡片优化

--- touhou-gamejam-quick-web/src/App.vue
+++ touhou-gamejam-quick-web/src/App.vue
@@ -4,27 +4,21 @@
       <el-header>标题</el-header>
       <el-main>
         <div v-for="item in 5" :key="item" class="main-box">
-          <el-row>
-            <el-col :span="8">
-              <div class="grid-content bg-purple">
-                <div class="des-img">
-                  <img class="box-img" src="../public/testimg.png">
+            <el-card class="box-card" body-style="{padding: '0px'}" >
+              <template #header>
+                <!--标题-->
+                <div class="card-header">
+                  <h2>《这是很长很长很长的标题》</h2>
                 </div>
+              </template>
+              <!--图片div-->
+              <div class="head-img">
+                <div class="box-img">
+                  <img class="text-img" src="../public/test.png">
+                </div>
+
               </div>
-            </el-col>
-            <el-col :span="12">
-              <div class="grid-content bg-purple-light">
-                <br>
-                这是一段文字这是一段文字这是一段文字这是一段文字这是一段文字这是一段文字这是一段文字这是一段文字这是一段文字这是一段文字这是一段文字这是一段文字这是一段文字这是一段文字这是一段文字这是一段文字这这是一段文字这是一段文字这一段文字这这是一段文字这是一段文字这
-              </div>
-            </el-col>
-            <el-col :span="4">
-              <div class="grid-content bg-purple">
-                <el-button class="button" type="primary" plain>download</el-button>
-                <el-button class="button" type="success" plain>des</el-button>
-              </div>
-            </el-col>
-          </el-row>
+            </el-card>
         </div>
       </el-main>
     </el-container>
@@ -41,52 +35,55 @@ export default {
 </script>
 
 <style>
+.head-img{
+  width: 100%;
+  height: 50%;
+}
+
+.el-card__body{
+  height: 80%;
+}
+.box-card {
+  margin: 0 0 30px;
+  width: 100%;
+  height: 800px;
+}
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
 .el-header{
   background-color: #b3c0d1;
   color: var(--el-text-color-primary);
   text-align: center;
   line-height: 60px;
-  width: 80%;
+  width: 70%;
   margin: 0 auto;
 }
 .el-main {
   background-color: #e9eef3;
   color: var(--el-text-color-primary);
   text-align: center;
-  width: 80%;
+  width: 70%;
   margin: 0 auto;
   height: 100%;
 }
-
+.text-img{
+  width: 100%;
+  height: 100%;
+  border-radius: 5%;
+}
 .box-img{
+  float: left;
   border-radius: 10px;
-  width: 100%;
+  width: 50%;
   height: 100%;
 }
-.des-img{
-  width: 100%;
-  height: 100%;
-}
-.bg-purple {
-  background: #d3dce6;
-}
-.bg-purple-light {
-  background: #e5e9f2;
-}
-.button{
-  height: 50%;
-  width: 100%;
-  float: right;
-}
-.grid-content {
-  text-align: justify;
-  width: 100%;
-  border-radius: 4px;
-  height: 160px;
-}
+
 .main-box{
   height: 100%;
   width: 100%;
-  margin: 10px;
 }
 </style>
