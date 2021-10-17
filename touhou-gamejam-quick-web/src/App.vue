22aee1964251290469d4a69faae61d291cd4ff8f
null122 <71970238+null122@users.noreply.github.com>
2021-10-17 14:17:50 +0800

    前端

--- touhou-gamejam-quick-web/src/App.vue
+++ touhou-gamejam-quick-web/src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div class="common-layout">
-    <el-container>
-      <el-header>标题</el-header>
-      <el-main>
-        <div v-for="item in 5" :key="item" class="main-box">
-            <el-card class="box-card" body-style="{padding: '0px'}" >
-              <template #header>
-                <!--标题-->
-                <div class="card-header">
-                  <h2>《这是很长很长很长的标题》</h2>
-                </div>
-              </template>
-              <!--图片div-->
-              <div class="head-img">
-                <div class="box-img">
-                  <img class="text-img" src="../public/test.png">
-                </div>
-
-              </div>
-            </el-card>
+  <el-header>标题</el-header>
+  <el-container>
+    <el-main >
+      <el-card class="box-card main">
+        <!--头部-->
+        <template #header>
+          <div class="card-header">
+            <span>名字</span>
+          </div>
+        </template>
+        <!--走马灯-->
+        <div class="card-img">
+          <el-carousel trigger="click" height="350px">
+            <el-carousel-item v-for="item in 1" :key="item">
+              <h3 class="small">{{ item }}</h3>
+            </el-carousel-item>
+          </el-carousel>
         </div>
-      </el-main>
-    </el-container>
-  </div>
+      </el-card>
+    </el-main>
+    <el-aside width="30%">
+
+    </el-aside>
+  </el-container>
 </template>
 
 <script>
@@ -35,55 +35,75 @@ export default {
 </script>
 
 <style>
-.head-img{
-  width: 100%;
-  height: 50%;
-}
 
-.el-card__body{
-  height: 80%;
+.card-img{
+  height: 30%;
+  width: 100%;
 }
 .box-card {
-  margin: 0 0 30px;
-  width: 100%;
-  height: 800px;
+  width: 480px;
 }
 .card-header {
+  height: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
-
+.main{
+  width: 100%;
+  height: 1000px;
+}
+.el-container{
+  margin: 0 auto;
+  width: 70%;
+}
 .el-header{
   background-color: #b3c0d1;
   color: var(--el-text-color-primary);
   text-align: center;
   line-height: 60px;
-  width: 70%;
   margin: 0 auto;
+  width: 70%;
 }
+
+.el-aside {
+  background-color: #d3dce6;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 200px;
+}
+
 .el-main {
   background-color: #e9eef3;
   color: var(--el-text-color-primary);
   text-align: center;
-  width: 70%;
-  margin: 0 auto;
-  height: 100%;
+  line-height: 160px;
 }
-.text-img{
-  width: 100%;
-  height: 100%;
-  border-radius: 5%;
+.el-card__header{
+  height: 10%;
 }
-.box-img{
-  float: left;
-  border-radius: 10px;
-  width: 50%;
-  height: 100%;
+.el-card__body{
+  height: 85%;
 }
 
-.main-box{
-  height: 100%;
-  width: 100%;
+
+
+
+
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
 }
 </style>
