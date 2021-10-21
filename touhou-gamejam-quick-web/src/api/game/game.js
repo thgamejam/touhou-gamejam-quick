a633a78d680fba406cda9893c7f4e4f7c1714754
strluck <s@strluck.com>
2021-10-21 18:35:23 +0800

    前端 添加api.getGameDownloadURL和api.getGameList

--- touhou-gamejam-quick-web/src/api/game/game.js
+++ touhou-gamejam-quick-web/src/api/game/game.js
@@ -2,6 +2,12 @@ import axios from "axios";
 
 const getGame = id => axios.get(`/api/v1/game/${id}`).then(res => res.data);
 
+const getGameDownloadURL = id => axios.get(`/api/v1/game/download/${id}`).then(res => res.data);
+
+const getGameList = page => axios.get(`/api/v1/game/list/${page}`).then(res => res.data);
+
 export {
-  getGame
+  getGame,
+  getGameDownloadURL,
+  getGameList,
 }
