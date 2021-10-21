e4b7726f0b3abc1017c5207af621991e0314bab9
null122 <71970238+null122@users.noreply.github.com>
2021-10-21 20:35:32 +0800

    前端优化

--- touhou-gamejam-quick-web/src/router/index.js
+++ touhou-gamejam-quick-web/src/router/index.js
@@ -1,5 +1,5 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import Home from '../App.vue'
+// import Home from '../App.vue'
 import Game from "@/views/page/Game.vue";
 import GameJam from "@/views/page/GameJam.vue";
 import Test from '@/views/component/GameExhibit.vue'
@@ -9,8 +9,7 @@ import Test3 from '@/views/component/GameDownload.vue'
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: GameJam
   },
   {
     path: '/game/:id',
--- touhou-gamejam-quick-web/src/views/page/GameJam.vue
+++ touhou-gamejam-quick-web/src/views/page/GameJam.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="body-box">
     <div class="header" :style="'height'+imgHeight+'px'">
-      <img :style="'height'+imgHeight+'px'" src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.daimg.com%2Fuploads%2Fallimg%2F190228%2F3-1Z22Q15139.jpg&refer=http%3A%2F%2Fimg.daimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1637385219&t=00c6b4cf242f7463cd03d941af9f7dcc">
+      <img class="gamejam-img" :style="'height'+imgHeight+'px'" src="http://test.strluck.com:9000/public-download/logo.jpg">
     </div>
-    <div style="width: 100%">
-      <el-space v-for="item in gamelist.length/3" :key="item" wrap>
-        aaaaaaaaaaaaaaa
-        <!--        <div v-for="game in item*3>gamelist.length?gamelist.length:item*3" :key="game">-->
-        <!--          <img :src="gamelist[game]">-->
-        <!--        </div>-->
-      </el-space>
+    <div v-for="item in gamelist" :key="item" class="header"  :style="'height'+imgHeight+'px'">
+      <a :href="'/game/'+item.id">
+        <img class="gamejam-img" :src="item.img" :style="'height'+imgHeight+'px'">
+      </a>
     </div>
   </div>
 </template>
@@ -20,12 +17,12 @@ export default {
     return{
       gamelist:[
         {
-          id:0,
+          id:1,
           img:"https://img1.baidu.com/it/u=1514254201,1610764859&fm=26&fmt=auto",
           name:"aaa",
         },
         {
-          id:1,
+          id:2,
           img:"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic18.nipic.com%2F20111216%2F9047182_205018652177_2.jpg&refer=http%3A%2F%2Fpic18.nipic.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1637387402&t=4a63f0b795e435b8e24294b63bf4562c",
           name:"bbb",
         },
@@ -35,7 +32,6 @@ export default {
   },
   created() {
     this.setHeight();
-    console.log(this.gamelist.length)
   },
   methods:{
     setHeight(){
@@ -58,7 +54,7 @@ export default {
 .header{
   width: 100%;
 }
-.el-main {
+.gamejam-main {
   background-color: #e9eef3;
   color: var(--el-text-color-primary);
 }
