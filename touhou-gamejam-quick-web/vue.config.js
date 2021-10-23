a750cd097ad2aa072de3d4d7a2fc5a3a6c100317
null122 <71970238+null122@users.noreply.github.com>
2021-10-23 20:46:16 +0800

    编译bug

--- touhou-gamejam-quick-web/vue.config.js
+++ touhou-gamejam-quick-web/vue.config.js
@@ -2,5 +2,19 @@ module.exports = {
   publicPath:  '/',
   devServer: {
     proxy: 'http://localhost:8000'
+  },
+  configureWebpack: {
+    performance: {
+      hints:'warning',
+      //入口起点的最大体积
+      maxEntrypointSize: 50000000,
+      //生成文件的最大体积
+      maxAssetSize: 30000000,
+      //只给出 js 文件的性能提示
+      assetFilter: function(assetFilename) {
+        return assetFilename.endsWith('.js');
+      }
+    }
   }
+
 };
